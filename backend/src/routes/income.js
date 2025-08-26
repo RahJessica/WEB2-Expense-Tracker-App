595ec869eca35b39740904757634d3d562de5a78
RahJess <jessicakantotiana@gmail.com>
2025-08-26 10:57:46 +0200

    fix: import function deleteIncome in route income.js

--- backend/src/routes/income.js
+++ backend/src/routes/income.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createIncome, getIncomes } = require('../controllers/incomesController.js');
+const { createIncome, getIncomes, deleteIncome } = require('../controllers/incomesController.js');
 const { authenticate } = require('../middlewares/authentification.js');
 
 router.use(authenticate);
 
 router.post('/new', createIncome);
 router.get('/', getIncomes);
-router.delete('/:id', deleteIncome);
+router.delete('/delete/:id', deleteIncome);
 
 module.exports = router;
