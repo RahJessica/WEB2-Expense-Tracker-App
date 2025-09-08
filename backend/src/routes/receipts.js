4c98f900cb53bbf9046d3b1bb52a5a08e9bd8843
nelio-gio <hei.rocques@gmail.com>
2025-09-08 14:28:39 +0300

    add:new file for upload fonctionality

--- backend/src/routes/receipts.js
+++ backend/src/routes/receipts.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { createReceipt, getReceipts, deleteReceipt } = require('../controllers/receiptsController.js');
 const { authenticate } = require('../middlewares/authentification.js');
+const upload = require('../middlewares/upload.js');
 
-router.use(authenticate);
+router.use(authenticate); 
 
-router.post('/new', createReceipt);       
-router.get('/', getReceipts);             
-router.delete('/:id', deleteReceipt);     
+// Nouveau téléversement avec fichier
+router.post('/new', upload.single('file'), createReceipt);
+router.get('/', getReceipts);
+router.delete('/:id', deleteReceipt);
 
 module.exports = router;
