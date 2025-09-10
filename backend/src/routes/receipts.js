4d9297b0b1c4da9099d26d2b536c4c90053f317a
RahJess <jessicakantotiana@gmail.com>
2025-09-10 23:19:18 +0200

    add: token synchronization with localStorage in App.jsx

--- backend/src/routes/receipts.js
+++ backend/src/routes/receipts.js
@@ -8,5 +8,7 @@ router.use(authenticate);
 router.post('/new', createReceipt);       
 router.get('/', getReceipts);             
 router.delete('/:id', deleteReceipt);     
+const upload = require('../middlewares/upload.js'); // <== importe multer
+router.post('/new', upload.single('file'), createReceipt);  // <== ajoute upload.single('file')
 
 module.exports = router;
