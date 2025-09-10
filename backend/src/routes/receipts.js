a09bb03b265c012cd24faeef12e4caa55ccc93c4
nelio-gio <hei.rocques@gmail.com>
2025-09-10 22:46:42 +0300

    add:changes in receipt.js(routes)

--- backend/src/routes/receipts.js
+++ backend/src/routes/receipts.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { createReceipt, getReceipts, deleteReceipt } = require('../controllers/receiptsController.js');
 const { authenticate } = require('../middlewares/authentification.js');
+const upload = require('../middlewares/upload.js'); 
 
 router.use(authenticate);
 
-router.post('/new', createReceipt);       
+router.post('/new', upload.single('file'), createReceipt);    
 router.get('/', getReceipts);             
 router.delete('/:id', deleteReceipt);     
 
